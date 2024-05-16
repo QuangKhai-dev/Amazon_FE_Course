6145e4f70abb9d8b23ff85d55a0eac4e354e1bdc
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-05-16 16:14:21 +0700

    3-4 cau hinh sass và radix UI, ket hop cai dat thu vien formik va yup

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
+import { Button, Flex, Text } from "@radix-ui/themes";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :</Text>
+        <Button>Let's go</Button>
+      </Flex>
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amazon: {
+          blue: "#131921",
+          gray: "#E3E6E6",
+          yellow: "#ffd814",
+          green: "#7FDA69",
+          white: "#ffffff",
+          black: "#000000",
+          orange: "#ffa41c",
+        },
+      },
+    },
   },
   plugins: [],
 };
