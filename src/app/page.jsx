721f68360eabcd82fb4a49db18572e02b2a24adf
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-05-20 15:30:09 +0700

    Demo Global Css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,10 @@ import { Button, Flex, Text } from '@radix-ui/themes';
 export default function Home() {
   return (
     <main>
+      <h2 className="category_title">Hello CyberSoft</h2>
+      <a href="https://google.com" target="_blank" rel="noreferrer">
+        Click me
+      </a>
       <Flex direction="column" gap="2">
         <Text>Hello from Radix Themes :</Text>
         <Button>Let's go</Button>
